1b3610b33cc1ffcd8a8369c13a4d754a22101c09
Murat SAYRIM <muratsayrim@hotmail.com.tr>
2023-09-15 13:49:44 +0300

    initial

---
+++ src/main/java/com/example/stajproject1/StajProject1Application.java
@@ -0,0 +1,13 @@
+package com.example.stajproject1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StajProject1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(StajProject1Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/stajproject1/StajProject1ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.stajproject1;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StajProject1ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
